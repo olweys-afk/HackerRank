929dfae13f2f93f7ced3e60062c2afa042bd57db
olweys-afk <63777147+olweys-afk@users.noreply.github.com>
2020-04-22 05:51:28 +0800

    Create If_Else.py

---
+++ If_Else.py
@@ -0,0 +1,16 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+if (n in range (6, 21) or n % 2 != 0):
+        print("Weird")
+else:
+        print ("Not Weird")
