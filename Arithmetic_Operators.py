b07df938bf07f2a4a10fa2c572febfcf3c816bb4
olweys-afk <leonardoluisicasiano@gmail.com>
2020-04-25 01:57:44 +0800

    hackerrank pt.3

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,10 @@
+#The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+#The third line contains the product of the two numbers.
+
+Fir = int(input())
+Sec = int(input())
+
+print(Fir + Sec)
+print(Fir - Sec)
+print(Fir * Sec)
